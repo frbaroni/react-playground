f1e122be2d4bc5c956168a0380817d1003ea70dc
Fernando Rodrigues Baroni <fernando@baroni.tech>
2018-03-03 02:45:33 -0300

    Test kubernetes env

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <LandingPage />
+        Hello World, {process.env.REACT_APP_MESSAGE}
       </Provider>
     );
   }
